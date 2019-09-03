2b2fb4b2214e83165ed65b91730fc53f1b9362f2
naganathpawar <nageshpawar21@gmail.com>
2019-09-03 16:27:36 +0100

    code coverage

--- src/main/java/com/mef/filter/main/util/CommonUtility.java
+++ src/main/java/com/mef/filter/main/util/CommonUtility.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import com.mef.filter.main.config.AppProperties;
+import com.sun.codemodel.internal.JCommentPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,91 +23,110 @@ import javax.validation.constraints.NotNull;
 
 /**
  * @author naganathpawar
- *
  */
 @Service
 public class CommonUtility {
-	@Autowired
-	AppProperties appProperties;
-	public static final Logger logger = LoggerFactory.getLogger(CommonUtility.class);
-/**
- * @param filePath
- * @param value
- * @return
- * @throws IOException
- */
-public  String  createFile(@NotNull String filePath,@NotNull String value) throws IOException {
+    /**
+     *
+     */
+    @Autowired
+    AppProperties appProperties;
+    /**
+     *
+     */
+    public static final Logger logger = LoggerFactory.getLogger ( CommonUtility.class );
 
-	if (new File(filePath).exists()) {
-		Path path = Paths.get(filePath);
-		Files.delete(path);
-	}
-		FileWriter writer=new FileWriter(filePath,false);
-		PrintWriter printWriter=new PrintWriter(writer);
-		printWriter.printf("%s"+"%n", value);
-		printWriter.close();
-		
-		return filePath;
-		
-	}
+    /**
+     * @param filePath
+     * @param value
+     * @return
+     * @throws IOException
+     */
+    public String createFile ( @NotNull String filePath , @NotNull String value ) throws IOException {
 
-/*************************************
- * @param path
- * @param folderName
- * @return
- */
-public String createFolder(@NotNull String path, @NotNull String folderName){
-	String csvFile = null;
-	String folderPath=path.concat(folderName);
+        if ( new File ( filePath ).exists ( ) ) {
+            Path path = Paths.get ( filePath );
+            Files.delete ( path );
+        }
+        FileWriter writer = new FileWriter ( filePath , false );
+        PrintWriter printWriter = new PrintWriter ( writer );
+        printWriter.printf ( "%s" + "%n" , value );
+        printWriter.close ( );
 
-	if (new File(folderPath).exists()) {
-		deleteDirectoryStream(Paths.get(folderPath));
-		if (!new File(folderPath).exists() && new File(folderPath).mkdir()) {
-			csvFile = folderPath;
-		}
-	}else {
-		if(new File(folderPath).mkdir())
-			csvFile = folderPath;
-	}
-	return csvFile;
-}
+        return filePath;
 
-/**
- * @param filePath
- * @return
- */
-public String getPath(String filePath) {
-	String[] strArr =filePath.split("/");
-	return filePath.replace(strArr[strArr.length-1], "");
-}
+    }
 
-/**
- * @param files
- * @return
- */
-public List<String> getIds(String files) {
-	String[] str = files.split(",");
+    /*************************************
+     * @param path
+     * @param folderName
+     * @return
+     */
+    public String createFolder ( @NotNull String path , @NotNull String folderName ) {
+        String folderPath = path.concat ( folderName );
+        return new File ( folderPath ).exists ( ) ? deleteDirectoryStream ( folderPath ) : createDirectory ( folderPath );
 
-	return Arrays.asList(str);
-}
-	void deleteDirectoryStream(Path path) {
-		Stream<Path> pathStream =null;
-		try {
-			pathStream= Files.walk(path);
-			pathStream.sorted(Comparator.reverseOrder())
-					.map(Path::toFile)
-					.forEach(File::delete);
-		}catch (IOException e){
-			logger.error("deleteDirectoryStream",e);
-		}finally {
-			pathStream.close();
-		}
-	}
+    }
 
-	public String getSelectionType(@NotNull String value){
-	if (value.equalsIgnoreCase(appProperties.getCommercialBundle() ))
-		return appProperties.getInputType().get(0);
-		else
-			return appProperties.getInputType().get(1);
-	}
+    /**
+     * @param filePath
+     * @return
+     */
+    public String getPath ( String filePath ) {
+        String[] strArr = filePath.split ( "/" );
+        return filePath.replace ( strArr[ strArr.length - 1 ] , "" );
+    }
+
+    /**
+     * @param files
+     * @return
+     */
+    public List < String > getIds ( String files ) {
+        String[] str = files.split ( "," );
+
+        return Arrays.asList ( str );
+    }
+
+
+    /**
+     * @param folderPath
+     * @return
+     */
+    private String deleteDirectoryStream ( String folderPath ) {
+        Stream < Path > pathStream = null;
+        try {
+            pathStream = Files.walk ( Paths.get ( folderPath ) );
+            pathStream.sorted ( Comparator.reverseOrder ( ) )
+                    .map ( Path :: toFile )
+                    .forEach ( File :: delete );
+        } catch ( IOException e ) {
+            logger.error ( "deleteDirectoryStream" , e );
+        } finally {
+            pathStream.close ( );
+        }
+        return createDirectory ( folderPath );
+
+    }
+
+    /**
+     * @param value
+     * @return
+     */
+    public String getSelectionType ( @NotNull String value ) {
+        if ( value.equalsIgnoreCase ( appProperties.getCommercialBundle ( ) ) )
+            return appProperties.getInputType ( ).get ( 0 );
+        else
+            return appProperties.getInputType ( ).get ( 1 );
+    }
+
+    /**
+     * @param folderPath
+     * @return
+     */
+    private String createDirectory ( @NotNull String folderPath ) {
+
+        if ( new File ( folderPath ).mkdir ( ) ) return folderPath;
+        else return null;
+
+    }
 }
